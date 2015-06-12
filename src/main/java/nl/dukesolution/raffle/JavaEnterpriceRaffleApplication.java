b1e4bc993209d9947a0ba96ca28da97c4c78bd1a
bart <bart.kors@gmail.com>
2015-06-12 19:47:56 +0200

    added jboss support

--- src/main/java/nl/dukesolution/raffle/JavaEnterpriceRaffleApplication.java
+++ src/main/java/nl/dukesolution/raffle/JavaEnterpriceRaffleApplication.java
@@ -2,9 +2,10 @@ package nl.dukesolution.raffle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaEnterpriceRaffleApplication {
+public class JavaEnterpriceRaffleApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaEnterpriceRaffleApplication.class, args);
